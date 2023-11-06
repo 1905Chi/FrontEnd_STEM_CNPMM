1b8ea21c671a2a99bba4bf855d51fae61ed91959
doanquocbao <doanquocbao4117@gmail.com>
2023-11-06 18:50:31 +0700

    create landing page, topbar, footer

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<div className="footer-container">
+			<div className="item-footer">
+				<h4>
+					Tổng Công ty Giải pháp doanh nghiệp Viettel Chi nhánh Tập đoàn Công nghiệp - Viễn thông Quân đội Số
+					1 đường Trần Hữu Dực, P. Mỹ Đình 2, Q. Nam Từ Liêm, TP. Hà Nộ
+				</h4>
+			</div>
+			<div className="item-footer">
+				<h1>Liên kết</h1>
+				<h4>Giới thiệu</h4>
+				<h4>Quy chế hoạt động</h4>
+				<h4>Điều khoản sử dụng</h4>
+				<h4>Chính sách bảo mật</h4>
+				<h4>Quyền lợi thành viên</h4>
+			</div>
+			<div className="item-footer">
+				<h1>18008000 nhánh 2</h1>
+				<h4>Hotline: 19001009</h4>
+				<h4>viettelstudy@viettel.com.vn</h4>
+				<h4>https://viettelstudy.vn</h4>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
