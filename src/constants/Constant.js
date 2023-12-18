aaa7a0c4ba8ce7c1944c13cee8d717cc5760413e
doanquocbao <doanquocbao4117@gmail.com>
2023-12-18 21:09:34 +0700

    fix api

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -1,2 +1,2 @@
-export const url = "http://localhost:8080/";
-export const anh_logo='https://res.cloudinary.com/djzwxw0ao/image/upload/v1696942528/uqbxidtwcdbqn8glt6we.jpg'
+export const url = 'http://172.16.16.70:8080/';
+export const anh_logo = 'https://res.cloudinary.com/djzwxw0ao/image/upload/v1696942528/uqbxidtwcdbqn8glt6we.jpg';
